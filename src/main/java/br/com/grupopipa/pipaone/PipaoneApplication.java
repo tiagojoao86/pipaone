5d6b1f71d5d55ac281cb98521ef1ce6a4815af9a
Tiago J Pereira <pereira.tiagojoao@gmail.com>
2024-05-01 08:30:23 -0300

    initial commit

---
+++ src/main/java/br/com/grupopipa/pipaone/PipaoneApplication.java
@@ -0,0 +1,13 @@
+package br.com.grupopipa.pipaone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipaoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipaoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/groupopipa/pipaone/PipaoneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.groupopipa.pipaone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipaoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
